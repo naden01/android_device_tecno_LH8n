c5bbd37a6195869fc75d6dcec11adfa427007c54
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-08-31 02:16:33 -0700

    drop it

--- lineage_FULL-64-ARMV82.mk
+++ lineage_FULL-64-ARMV82.mk
@@ -14,14 +14,15 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from FULL-64-ARMV82 device
 $(call inherit-product, device/tecno/FULL-64-ARMV82/device.mk)
 
-PRODUCT_DEVICE := FULL-64-ARMV82
-PRODUCT_NAME := lineage_FULL-64-ARMV82
+PRODUCT_MODEL := TECNO LH8n
+PRODUCT_DEVICE := LH8n
+PRODUCT_NAME := lineage_LH8n
 PRODUCT_BRAND := TECNO
-PRODUCT_MANUFACTURER := tecno
+PRODUCT_MANUFACTURER := TECNO
 
 PRODUCT_GMS_CLIENTID_BASE := android-transsion
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sys_tssi_64_armv82_tecno-user 14 UP1A.231005.007 651371 release-keys"
+    PRODUCT_NAME=LH8n-GL
 
 BUILD_FINGERPRINT := TECNO/TSSI/FULL-64-ARMV82:14/UP1A.231005.007/240703V382:user/release-keys
