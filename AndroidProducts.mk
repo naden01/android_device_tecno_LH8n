6b72b3c6e2096be9fb89b149f785749e35d56e21
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-08-31 02:22:01 -0700

    drop it

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_FULL-64-ARMV82.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_FULL-64-ARMV82-user \
-    lineage_FULL-64-ARMV82-userdebug \
-    lineage_FULL-64-ARMV82-eng
+    $(LOCAL_DIR)/lineage_LH8n.mk
